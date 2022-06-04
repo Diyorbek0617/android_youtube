32c67b00439b4806e3f9aa2839884726b553d669
Diyorbek0617 <nizomiddinov.d.d@gmail.com>
2022-06-04 12:22:40 +0500

    Initial commit

---
+++ app/src/main/java/com/fam/android_youtube/model/Feed.kt
@@ -0,0 +1,5 @@
+package com.fam.android_youtube
+
+data class Feed(var profile:Int, var photo:Int) {
+
+}
---
+++ app/src/main/java/com/fam/android_youtube/model/Filter.kt
@@ -0,0 +1,5 @@
+package com.fam.android_youtube
+
+data class Filter(var title:String) {
+
+}
